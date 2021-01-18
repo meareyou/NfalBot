fca4bf83fa3a4086f853237693844ec5397e6224
Catnime <77617996+Catnime@users.noreply.github.com>
2021-01-18 23:16:46 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -27,7 +27,7 @@ for module_name in ALL_MODULES:
 
 
 LOGS.info(
-    f"🔥 XBOT-REMIX 🔥 V{BOT_VER} [SUCCESSFUL ACTIVATED! ⚙️]")
+    f"✧ NFALBOT ✧ V{BOT_VER} [SUCCESSFUL ACTIVATED! ⚙️]")
 
 
 if len(argv) not in (1, 3, 4):
